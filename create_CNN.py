5e366ab90c5edffdf1f2563a79a9b6423598cbf4
ronca <roncalara@outlook.com>
2021-06-03 00:42:21 -0700

    Ram usage of create_CNN.py reduced by half, bug that goes to 99% ram usage as been fixed. 50 lines of code were cut due to using a built in function for loading in data.

--- create_CNN.py
+++ create_CNN.py
@@ -1,56 +1,14 @@
-import numpy as np
-from keras.preprocessing import image
 from keras.models import Sequential
 from keras.applications import mobilenet
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Flatten
 from pathlib import Path
 from tensorflow import keras
+import tensorflow as tf
 
 
-# Function for creating our array of images and labels to feed into model
-def img_array_and_labels(images,
-                         labels,
-                         folder_path,
-                         class_num):
-
-    for img in folder_path.glob("*.png"):
-        img = image.load_img(img,
-                             color_mode="rgb")
-
-        image_array = image.img_to_array(img)
-        images.append(image_array)
-
-        labels.append(class_num)
-
-    return images, labels
-
-
-# We can use this function to create our own ConvNet
-def generate_model():
-    # Create a model and add layers
-    model = Sequential()
-
-    # Add Convolutional layers to convert image data into feature data
-    model.add(Conv2D(32, (3, 3),
-                     padding="same",
-                     activation="relu",
-                     input_shape=(224, 224, 3)))
-    model.add(Conv2D(32, (5, 5), activation="relu"))
-    model.add(Conv2D(32, (5, 5), activation="relu"))
-    # MaxPooling to reduce size and complexity of feature data before classification
-    model.add(MaxPooling2D(pool_size=(4, 4)))
-    # randomly cut 25% of neural network
-    model.add(Dropout(0.25))
-
-    # Feature data needs to be converted to 1D vector
-    # before going into classification layers.
-    model.add(Flatten())
-
-    # Classification layers
-    model.add(Dense(1024, activation="relu"))
-    model.add(Dense(3, activation="softmax"))
-
-    return model
+def process(image_file, label):
+    image_file = tf.cast(image_file/255, tf.float32)
+    return image_file, label
 
 
 # This function is used for transfer learning with an existing neural net
@@ -104,57 +62,21 @@ def generate_model_TF_fine_tune():
 
 def main():
 
-    data_set_folder = Path("data_set")
+    training_data = tf.keras.preprocessing.image_dataset_from_directory("data_set/",
+                                                                        image_size=(224, 224),
+                                                                        label_mode="categorical",
+                                                                        shuffle=True,
+                                                                        class_names=["normal_images",
+                                                                                     "under_extruded_images",
+                                                                                     "over_extruded_images",
+                                                                                     "no_pattern"])
 
-    # Training data folders
-    normal_images = data_set_folder / "normal_images"
-    under_extruded_images = data_set_folder / "under_extruded_images"
-    over_extruded_images = data_set_folder / "over_extruded_images"
-    no_pattern_images = data_set_folder / "no_pattern"
-    td_images = []
-    td_labels = []
-
-    # Load all normal training images
-    td_images, td_labels = img_array_and_labels(td_images,
-                                                td_labels,
-                                                normal_images,
-                                                class_num=0)
-
-    # Load all under extruded training images
-    td_images, td_labels = img_array_and_labels(td_images,
-                                                td_labels,
-                                                under_extruded_images,
-                                                class_num=1)
-
-    # Load all over extruded training images
-    td_images, td_labels = img_array_and_labels(td_images,
-                                                td_labels,
-                                                over_extruded_images,
-                                                class_num=2)
-
-    td_images, td_labels = img_array_and_labels(td_images,
-                                                td_labels,
-                                                no_pattern_images,
-                                                class_num=3)
-
-    # Numpy is usually used to deal with multi dimensional arrays properly
-    x_train = np.array(td_images)
-
-    # Setting up our class labels for keras
-    y_train = np.array(td_labels)
-    y_train = keras.utils.to_categorical(y_train, 4)
-
-    # Normalize data set to values between 0 and 1
-    x_train = x_train / 255
-
-    # We call one of the CNN methodologies
+    training_data = training_data.map(process)
 
     # with TL, x_train and x_val will be feature data and not image data
     # Since image data was converted to feature data, the model
     # will ONLY consist of the classification layers for this
     # model, x_train, x_val = generate_model_TL(x_train)
-
-    # model = generate_model()
 
     model, base_model = generate_model_TF_fine_tune()
     base_model.trainable = True
@@ -171,10 +93,8 @@ def main():
 
     # Train the model
     model.fit(
-        x_train,
-        y_train,
-        epochs=10,
-        shuffle=True
+        training_data,
+        epochs=10
     )
 
     # Save neural network structure
