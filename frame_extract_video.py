16bfb0376c1789c042914fc56fc372b793ed1815
ronca <roncalara@outlook.com>
2021-06-06 23:37:10 -0700

    added ability for frame extractor to handle a list of video files and output to a list of multiple folders, more data added to 15%fill

--- frame_extract_video.py
+++ frame_extract_video.py
@@ -2,34 +2,54 @@ import cv2
 from pathlib import Path
 import numpy as np
 
-vid_file = "video_files/Princess_Leia_normal.mp4"
-vid_file_name = Path(vid_file).stem
-vid_cap = cv2.VideoCapture(vid_file)
-censor = True
 
-success, image = vid_cap.read()
-count = 0
-
-while success:
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = image[35:259, 128:352]
-
-    if censor:
-        # image = cv2.rectangle(image, (0, 0), (224, 45), (0, 0, 0), -1)
-        # diagonal boundry lines
-        image = cv2.line(image, (165, 45 - 45), (115, 95 - 45), (0, 0, 0), 10)
-        image = cv2.line(image, (45, 45 - 45), (95, 95 - 45), (0, 0, 0), 10)
-        # filling between diagonal lines
-        image = cv2.line(image, (45, 45 - 45), (165, 45 - 45), (0, 0, 0), 10)
-        image = cv2.line(image, (55, 55 - 45), (155, 55 - 45), (0, 0, 0), 10)
-        image = cv2.line(image, (65, 65 - 45), (145, 65 - 45), (0, 0, 0), 10)
-        image = cv2.line(image, (75, 75 - 45), (135, 75 - 45), (0, 0, 0), 10)
-        image = cv2.line(image, (85, 85 - 45), (125, 85 - 45), (0, 0, 0), 10)
-        image = cv2.line(image, (95, 95 - 45), (115, 95 - 45), (0, 0, 0), 10)
-
-    cv2.imwrite(fr"images_to_try\{vid_file_name}_{count}.png",
-                image)
+def video_to_frames(vid_file, output_folder):
+    vid_file_name = Path(vid_file).stem
+    vid_cap = cv2.VideoCapture(vid_file)
+    censor = True
 
     success, image = vid_cap.read()
-    print("Read a new frame: ", success)
-    count += 1
+    count = 0
+
+    while success:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = image[35:259, 128:352]
+
+        if censor:
+            # image = cv2.rectangle(image, (0, 0), (224, 45), (0, 0, 0), -1)
+            # diagonal boundry lines
+            image = cv2.line(image, (165, 45 - 45), (115, 95 - 45), (0, 0, 0), 10)
+            image = cv2.line(image, (45, 45 - 45), (95, 95 - 45), (0, 0, 0), 10)
+            # filling between diagonal lines
+            image = cv2.line(image, (45, 45 - 45), (165, 45 - 45), (0, 0, 0), 10)
+            image = cv2.line(image, (55, 55 - 45), (155, 55 - 45), (0, 0, 0), 10)
+            image = cv2.line(image, (65, 65 - 45), (145, 65 - 45), (0, 0, 0), 10)
+            image = cv2.line(image, (75, 75 - 45), (135, 75 - 45), (0, 0, 0), 10)
+            image = cv2.line(image, (85, 85 - 45), (125, 85 - 45), (0, 0, 0), 10)
+            image = cv2.line(image, (95, 95 - 45), (115, 95 - 45), (0, 0, 0), 10)
+
+        cv2.imwrite(f"{output_folder}/{vid_file_name}_{count}.png",
+                    image)
+
+        success, image = vid_cap.read()
+        print("Read a new frame: ", success)
+        count += 1
+
+
+def main():
+    video_files_list = ["video_files/potato2_15-_fill_normal.mp4",
+                        "video_files/potato2_15-_fill_over.mp4",
+                        "video_files/potato2_15-_fill_under.mp4"]
+
+    output_folders_list = ["data_set_15/normal_images",
+                           "data_set_15/over_extruded_images",
+                           "data_set_15/under_extruded_images"]
+
+    for vid_file, output_folder in zip(video_files_list,
+                                       output_folders_list):
+        video_to_frames(vid_file,
+                        output_folder)
+
+
+if __name__ == "__main__":
+    main()
